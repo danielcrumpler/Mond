5cb9fb93b452d93d6964362aa794b1caa958ccfe
Daniel Crumpler <dancrumpy@gmail.com>
2026-06-22 18:23:58 -0400

    v1.1.0
    Updated the weather to allow for manual or automatic weather and included logging. Fixed a issue with the wind throwing extra if statement errors. Updated how the weather icons are chosen to remove error of trying to open a non existent image.

--- @Resources/SwitchWeatherUnits.lua
+++ @Resources/SwitchWeatherUnits.lua
@@ -1,32 +1,117 @@
 function UpdateWeatherURL()
     local skin = SKIN
 
-    local TempSymbol = skin:GetVariable("TempSymbol")
-    local WindMeasurement = skin:GetVariable("WindMeasurement")
-    local lat = skin:GetMeasure("MeasureLatitude"):GetStringValue()
-    local lon = skin:GetMeasure("MeasureLongitude"):GetStringValue()
+    local modeText = skin:GetVariable("UseManualLocation", "0")
+    local useManual = tonumber(modeText) or 0
 
-    -- defaults
+    local lat
+    local lon
+    local source
+
+    if useManual == 1 then
+        source = "manual"
+
+        lat = tonumber(
+            skin:GetVariable("ManualLatitude", "")
+        )
+
+        lon = tonumber(
+            skin:GetVariable("ManualLongitude", "")
+        )
+    else
+        source = "automatic"
+
+        local latitudeMeasure =
+            skin:GetMeasure("MeasureLatitudeAuto")
+
+        local longitudeMeasure =
+            skin:GetMeasure("MeasureLongitudeAuto")
+
+        if not latitudeMeasure or not longitudeMeasure then
+            skin:Bang(
+                "!Log",
+                "Automatic location measures do not exist.",
+                "Error"
+            )
+            return
+        end
+
+        lat = tonumber(latitudeMeasure:GetStringValue())
+        lon = tonumber(longitudeMeasure:GetStringValue())
+    end
+
+    skin:Bang(
+        "!Log",
+        "Location mode: " .. source ..
+        " (UseManualLocation=" .. modeText .. ")",
+        "Notice"
+    )
+
+    if lat == nil or lon == nil then
+        skin:Bang(
+            "!Log",
+            source .. " weather location is not ready yet.",
+            "Notice"
+        )
+        return
+    end
+
+    local tempSymbol =
+        string.lower(skin:GetVariable("TempSymbol", "f"))
+
+    local windMeasurement =
+        string.lower(skin:GetVariable("WindMeasurement", "mph"))
+
     local tempUnit = "fahrenheit"
     local windUnit = "mph"
 
-    -- determine temperature unit
-    if TempSymbol == "C" or TempSymbol == "c" then
+    if tempSymbol == "c" then
         tempUnit = "celsius"
     end
 
-    -- determine wind unit
-    if WindMeasurement == "kph" or WindMeasurement == "kmh" or WindMeasurement == "KPH" then
+    if windMeasurement == "kph"
+        or windMeasurement == "kmh"
+        or windMeasurement == "km/h" then
         windUnit = "kmh"
-    else
-        windUnit = "mph"
     end
 
     local url = string.format(
         "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current_weather=true&temperature_unit=%s&windspeed_unit=%s&timezone=auto",
-        lat, lon, tempUnit, windUnit
+        tostring(lat),
+        tostring(lon),
+        tempUnit,
+        windUnit
     )
 
-    skin:Bang('!SetOption', 'MeasureWeather', 'URL', url)
-    skin:Bang('!CommandMeasure', 'MeasureWeather', 'Update')
+    skin:Bang(
+        "!Log",
+        "URL: " .. url ..
+        "Notice"
+    )
+
+    skin:Bang(
+        "!SetOption",
+        "MeasureWeather",
+        "URL",
+        url
+    )
+
+    skin:Bang(
+        "!UpdateMeasure",
+        "MeasureWeather"
+    )
+
+    skin:Bang(
+        "!CommandMeasure",
+        "MeasureWeather",
+        "Update"
+    )
+
+    skin:Bang(
+        "!Log",
+        "Weather URL updated using " .. source ..
+        " location: " .. tostring(lat) ..
+        ", " .. tostring(lon),
+        "Notice"
+    )
 end
--- @Resources/iconrouter.lua
+++ @Resources/iconrouter.lua
@@ -1,23 +1,66 @@
 function Initialize()
+    SetWeatherIcon("cloudy_day_night.png")
 end
 
 function Update()
-  local code = tonumber(SKIN:GetMeasure('MeasureWeatherCode'):GetStringValue()) or 0
-  local isDay = tonumber(SKIN:GetMeasure('MeasureIsDay'):GetStringValue()) or 1
-  local icons = {
-    [0]={'clear_day.png','clear_night.png'},
-    [1]={'fair_day.png','fair_night.png'},
-    [2]={'partly_cloudy_day.png','partly_cloudy_night.png'},
-    [3]={'cloudy_day_night.png','cloudy_day_night.png'},
-    [45]={'fog_day_night.png','fog_day_night.png'},
-    [48]={'fog_day_night.png','fog_day_night.png'},
-    [51]={'rain_day_night.png','rain_day_night.png'},
-    [61]={'rain_day_night.png','rain_day_night.png'},
-    [66]={'freezing_rain_day_night.png','freezing_rain_day_night.png'},
-    [71]={'snow_day_night.png','snow_day_night.png'},
-    [80]={'scattered_showers_day_night.png','scattered_showers_day_night.png'},
-    [95]={'thundershowers_day_night.png','thundershowers_day_night.png'}
-  }
-  local icon = icons[code] and icons[code][isDay==1 and 1 or 2] or 'cloudy_day_night.png'
-  return icon
+    local codeMeasure = SKIN:GetMeasure("MeasureWeatherCode")
+    local dayMeasure = SKIN:GetMeasure("MeasureIsDay")
+
+    local code = tonumber(codeMeasure:GetStringValue()) or -1
+    local isDay = tonumber(dayMeasure:GetStringValue()) or 1
+
+    local icons = {
+        [0]  = {"clear_day.png", "clear_night.png"},
+        [1]  = {"fair_day.png", "fair_night.png"},
+        [2]  = {"partly_cloudy_day.png", "partly_cloudy_night.png"},
+        [3]  = {"cloudy_day_night.png", "cloudy_day_night.png"},
+
+        [45] = {"fog_day_night.png", "fog_day_night.png"},
+        [48] = {"fog_day_night.png", "fog_day_night.png"},
+
+        [51] = {"rain_day_night.png", "rain_day_night.png"},
+        [53] = {"rain_day_night.png", "rain_day_night.png"},
+        [55] = {"rain_day_night.png", "rain_day_night.png"},
+        [56] = {"freezing_rain_day_night.png", "freezing_rain_day_night.png"},
+        [57] = {"freezing_rain_day_night.png", "freezing_rain_day_night.png"},
+
+        [61] = {"rain_day_night.png", "rain_day_night.png"},
+        [63] = {"rain_day_night.png", "rain_day_night.png"},
+        [65] = {"rain_day_night.png", "rain_day_night.png"},
+        [66] = {"freezing_rain_day_night.png", "freezing_rain_day_night.png"},
+        [67] = {"freezing_rain_day_night.png", "freezing_rain_day_night.png"},
+
+        [71] = {"snow_day_night.png", "snow_day_night.png"},
+        [73] = {"snow_day_night.png", "snow_day_night.png"},
+        [75] = {"snow_day_night.png", "snow_day_night.png"},
+        [77] = {"snow_day_night.png", "snow_day_night.png"},
+
+        [80] = {"scattered_showers_day_night.png", "scattered_showers_day_night.png"},
+        [81] = {"scattered_showers_day_night.png", "scattered_showers_day_night.png"},
+        [82] = {"scattered_showers_day_night.png", "scattered_showers_day_night.png"},
+
+        [85] = {"snow_day_night.png", "snow_day_night.png"},
+        [86] = {"snow_day_night.png", "snow_day_night.png"},
+
+        [95] = {"thundershowers_day_night.png", "thundershowers_day_night.png"},
+        [96] = {"thundershowers_day_night.png", "thundershowers_day_night.png"},
+        [99] = {"thundershowers_day_night.png", "thundershowers_day_night.png"}
+    }
+
+    local pair = icons[code]
+    local icon = "cloudy_day_night.png"
+
+    if pair then
+        icon = pair[isDay == 1 and 1 or 2]
+    end
+
+    SetWeatherIcon(icon)
+
+    return 0
+end
+
+function SetWeatherIcon(icon)
+    SKIN:Bang("!SetOption", "MeterCurrentIcon", "ImageName", icon)
+    SKIN:Bang("!UpdateMeter", "MeterCurrentIcon")
+    SKIN:Bang("!Redraw")
 end
